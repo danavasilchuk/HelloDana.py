0d247cd120238e1146881b72c1edc8bba2872512
Dana Vasilchuk <danavasilchuk@gmail.com>
2023-05-19 09:36:45 -0700

    1010 Class

--- combobox.py
+++ combobox.py
@@ -1,3 +1,14 @@
-#
+#import class library so that everytime its called, you can call it as tk
 import tkinter as tk
+from tkinter import ttk
+
+#function that handles the event
+def on_select(event):
+    #get the item that the end user has chosen on the combo box
+    selected_item = event.widget.get()
+    print ("Selected item", selected_item)
+
+#object for root window
+root = tk.Tk()
+root.title("Combobox Example")
 

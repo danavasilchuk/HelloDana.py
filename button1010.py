df1e98dcce369e47fc15a9b6616f236f18e90f5a
Dana Vasilchuk <danavasilchuk@gmail.com>
2023-05-19 09:06:20 -0700

    1010 class

--- button1010.py
+++ button1010.py
@@ -1,2 +1,23 @@
-#
+#import class library so that everytime its called, you can call it as tk
 import tkinter as tk
+
+#define a function to implement print function
+def button_click():
+    print("Button clicked!")
+
+#create root window object
+root = tk.Tk()
+root.title("Button Example")
+
+#create button object
+#three arguments in parameter; put button in root window, set the txt, 
+#set function in which button will click for 
+button = tk.Button(root,text="Click Me!", command=button_click)
+button.pack()
+
+#keep root window open; call root object
+root.mainloop()
+                       
+
+
+

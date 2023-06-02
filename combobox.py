ccdc586628eef8c177b07b2aac9600630b2c256c
danavasilchuk <130504712+danavasilchuk@users.noreply.github.com>
2023-06-01 21:29:17 -0700

    Update combobox.py

--- combobox.py
+++ combobox.py
@@ -12,3 +12,16 @@ def on_select(event):
 root = tk.Tk()
 root.title("Combobox Example")
 
+#Create static array of items
+items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
+#Tie it to combobox object we created
+combo_box = ttk.Combobox(root, values=items)
+
+#Bind combobox event to objects and tie it to method created earlier
+combo_box.bind("<<ComboboxSelected>>", on_select)
+
+combo_box.pack()
+
+root.mainloop()
+
